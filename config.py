e3008c730a2585169a40d55a1b38e44311a643c6
girnarimaharaj01-lang <girnarimaharaj01@gmail.com>
2026-01-14 12:01:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,21 +12,21 @@
 import os
 
 # Bot Token
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8346695576:AAErSTkMH1Jcb-yK0nE2gKCuSorGg0OhQMI")
 
 # Your API ID & Hash
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "29777466"))
+API_HASH = os.environ.get("API_HASH", "a04b3df726520026f207079aec2f9879")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "841851780"))
+ADMINS = int(os.environ.get("ADMINS", "8399557684"))
 
 # Your Mongodb Database Url
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "SaveRestricted")
 
 # Log Channel to Track New Users 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001889915480"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003164986113))
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then False
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
